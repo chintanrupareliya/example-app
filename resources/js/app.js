a74d9a44c9dc1a855d11067e3eb6b791d0f8a20d
Aldo Ferrari <aldoferrari.it@gmail.com>
2025-02-10 14:30:59 +0100

    added aboput page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
 import '../css/app.css';
 import './bootstrap';
-
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import router from './router';
+import i18n from './plugins/i18n';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,6 +20,8 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(router)
+            .use(i18n)
             .mount(el);
     },
     progress: {
